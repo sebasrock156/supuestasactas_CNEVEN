84a64539e20b891069307510928594f7ad7eb228
Camilo Montaño <42250720+sebasrock156@users.noreply.github.com>
2024-08-02 13:53:53 +0200

    Comentado, sin contar el nombre del script.

--- maduro_estupido_v2.sh
+++ maduro_estupido_v2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Mismo caso que en la versión 1.0, aquí la diferencia son las URLs de consulta, echenle ojo más abajo.
+
 if [ "$#" -ne 3 ]; then
   echo "You must enter exactly 3 numbers as arguments."
   exit 1
@@ -15,6 +17,9 @@ do
     random_number=$(( ( RANDOM * 32768 + RANDOM ) % (max - min + 1) + min ))
     base_url="https://gdp.theempire.tech/api/data?cdi=v${random_number}"
     # https://nodito01.democraciaactivas.com/api/Search/GetStates
+
+# Aquí tenemos dos nuevas URLs, la primera, se supone que es una base de datos que lanza como única muestra legible un "Hello World!" en su página de inicio)
+# La segunda, es el nodo de un host fuera de servicio que también contaba como base de datos, curioso el nombre de la URL. 
     
     response=$(curl -s "$base_url" )
     echo $response
@@ -30,6 +35,9 @@ do
         # fi
 
         # curl -s "$url" --output "./actas_raw/$filename"
+
+# En esta revisión del script, se omite por completo la verificación del número aleatorio.
+# Y, a diferencia del anterior, no dejaron una cédula autogenerada en el código.
 
     fi
 done
