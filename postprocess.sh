881f9a44a328d1351c0df3f7d59e0dacbc48b4e2
Camilo Montaño <42250720+sebasrock156@users.noreply.github.com>
2024-08-02 13:34:25 +0200

    Comentado

--- postprocess.sh
+++ postprocess.sh
@@ -1,10 +1,20 @@
+# Este es un script de postprocesado, se encarga de comprimir las imágenes de las actas -->
+# (no sé con qué motivo, supongo que para "optimizarlas" cuando no es necesario en Github)
+
+
 cp actas_raw/** actas
+#Copia las imágenes (se supone que en crudo) a la carpeta "actas"
+
 cd actas
+# Nos lleva a la carpeta "actas" con el comando "Change Directory"
+
 for file in *.jpg; do
   if [ -f "$file" ]; then
     echo "Processing $file"
     jpegoptim --size=85k $file
   fi
 done
+# Es un ciclo que hace un proceso de compresión en formato .jpeg
+
 
 cd ..
